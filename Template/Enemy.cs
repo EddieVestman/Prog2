39ab6e43ac493d262d732684e5cbc00d6f0da2a4
EddieVestman <33714818+EddieVestman@users.noreply.github.com>
2021-01-09 14:25:23 +0100

    v3

---
+++ Template/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Enemy
+    {
+        public Texture2D texture;
+        public Vector2 position;
+        public Vector2 velocity;
+
+        public bool isVisible = true;
+
+        Random random = new Random();
+        int randX, randY;
+
+    }
+}
